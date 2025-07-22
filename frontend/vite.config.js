ead64be9a61384b667b67bb0c7d2fc9d4127fd68
zhukata <zhukata@gmail.com>
2025-07-22 17:38:36 +0300

    init

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5002,
+    proxy: {
+      // Проксируем запросы к API
+      '/api': {
+        target: 'http://localhost:5001',
+      },
+      // Проксируем WebSocket соединения
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
+      },
+    },
+  },
+});
